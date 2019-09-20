90828266b56bffe245e69c97510872564b9a941b
extraErik <erygg01@live.com>
2019-09-20 12:15:27 -0500

    after lecture 195

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
 import { BehaviorSubject } from 'rxjs';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -45,7 +46,7 @@ export class PlacesService {
     return this._places.asObservable();
   }
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   getPlace(id: string) {
     return this.places.pipe(
@@ -57,6 +58,7 @@ export class PlacesService {
   }
 
   addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
+    let generatedId: string;
     const newPlace = new Place(
       Math.random().toString(),
       title,
@@ -67,10 +69,25 @@ export class PlacesService {
       dateTo,
       this.authService.userId
     );
-
-    return this.places.pipe(take(1), delay(1500), tap(places => {
-      this._places.next(places.concat(newPlace));
-    }));
+    return this.http.post<{name: string}>('https://maxionicplaces.firebaseio.com/offered-places.json', { ...newPlace, id: null })
+      .pipe(
+        switchMap(resData => {
+          generatedId = resData.name;
+          return this.places;
+        }),
+        take(1),
+        tap(places => {
+          newPlace.id = generatedId;
+          this._places.next(places.concat(newPlace));
+        })
+      );
+    // return this.places.pipe(
+    //   take(1),
+    //   delay(1500),
+    //   tap(places => {
+    //     this._places.next(places.concat(newPlace)
+    //   );
+    // }));
   }
 
   updatePlace(placeId: string, title: string, description: string) {
