89c2bf9a013903ac389334563f3abd1540851ab9
extraErik <erygg01@live.com>
2019-09-23 11:59:40 -0500

    after lecture 196

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -5,48 +5,88 @@ import { BehaviorSubject } from 'rxjs';
 import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
+// [
+//   new Place(
+//     'p1',
+//     'Manhattan Mansion',
+//     'In the heart of New York City',
+//     'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
+//     149.99,
+//     new Date('2020-01-01'),
+//     new Date('2020-12-31'),
+//     'xyz'
+//   ),
+//   new Place(
+//     'p2',
+//     'L\'Amour Toujours',
+//     'A romatic place in Paris',
+//     'https://www.parisperfect.com/g/photos/upload/sml_910679281-1496241966-romantic-retreats-vacation-rentals-paris.jpg',
+//     189.99,
+//     new Date('2020-01-01'),
+//     new Date('2020-12-31'),
+//     'abc'
+//   ),
+//   new Place(
+//     'p3',
+//     'The Foggy Palace',
+//     'Not your average city trip!',
+//     'https://cdn1.foap.com/images/71900230-994f-4d00-8200-304b40bf18c0/w640.jpg?1552030898',
+//     99.99,
+//     new Date('2020-01-01'),
+//     new Date('2020-12-31'),
+//     'abc'
+//   ),
+// ]
+
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City',
-      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
-      149.99,
-      new Date('2020-01-01'),
-      new Date('2020-12-31'),
-      'xyz'
-    ),
-    new Place(
-      'p2',
-      'L\'Amour Toujours',
-      'A romatic place in Paris',
-      'https://www.parisperfect.com/g/photos/upload/sml_910679281-1496241966-romantic-retreats-vacation-rentals-paris.jpg',
-      189.99,
-      new Date('2020-01-01'),
-      new Date('2020-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p3',
-      'The Foggy Palace',
-      'Not your average city trip!',
-      'https://cdn1.foap.com/images/71900230-994f-4d00-8200-304b40bf18c0/w640.jpg?1552030898',
-      99.99,
-      new Date('2020-01-01'),
-      new Date('2020-12-31'),
-      'abc'
-    ),
-  ]);
+  private _places = new BehaviorSubject<Place[]>([]);
 
   get places() {
     return this._places.asObservable();
   }
 
   constructor(private authService: AuthService, private http: HttpClient) { }
+
+  fetchPlaces() {
+    return this.http.get<{[key: string]: PlaceData }>('https://maxionicplaces.firebaseio.com/offered-places.json')
+    .pipe(
+      map(resData => {
+        const places = [];
+        for (const key in resData) {
+          if (resData.hasOwnProperty(key)) {
+            places.push(new Place(
+              key,
+              resData[key].title,
+              resData[key].description,
+              resData[key].imageUrl,
+              resData[key].price,
+              new Date(resData[key].availableFrom),
+              new Date(resData[key].availableTo),
+              resData[key].userId
+            ));
+          }
+        }
+        return places;
+      }),
+      delay(2000),
+      tap(places => {
+        this._places.next(places);
+      })
+    );
+  }
 
   getPlace(id: string) {
     return this.places.pipe(
