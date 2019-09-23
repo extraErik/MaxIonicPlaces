858414774704935ada2e30bded42fbbd45cf93a7
extraErik <erygg01@live.com>
2019-09-23 14:29:03 -0500

    after lecture 200

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
@@ -89,13 +89,24 @@ export class PlacesService {
   }
 
   getPlace(id: string) {
-    return this.places.pipe(
-      take(1),
-      map(places => {
-        return {...places.find(p => p.id === id)};
-      }))
-    ;
+    return this.http.get<PlaceData>(
+      `https://maxionicplaces.firebaseio.com/offered-places/${id}.json`
+    ).pipe(
+      map(placeData => {
+        return new Place(
+          id,
+          placeData.title,
+          placeData.description,
+          placeData.imageUrl,
+          placeData.price,
+          new Date(placeData.availableFrom),
+          new Date(placeData.availableTo),
+          placeData.userId
+        );
+      })
+    );
   }
+
 
   addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
     let generatedId: string;
@@ -134,6 +145,13 @@ export class PlacesService {
     let updatedPlaces: Place [];
     return this.places.pipe(
       take(1),
+      switchMap(places => {
+        if (!places || places.length <= 0) {
+          return this.fetchPlaces();
+        } else {
+          return of(places);
+        }
+      }),
       switchMap(places => {
         const updatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
         updatedPlaces = [...places];
