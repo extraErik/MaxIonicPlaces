4b476fa71894404733aa96d329cdac1e7ec801b0
extraErik <erygg01@live.com>
2019-09-25 08:55:08 -0500

    after lecture 203

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import { Booking } from './booking.model';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, pipe } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
-import { take, tap, delay } from 'rxjs/operators';
+import { take, tap, delay, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class BookingService {
   private _bookings = new BehaviorSubject<Booking[]>([]);
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   get bookings() {
     return this._bookings.asObservable();
@@ -25,6 +26,7 @@ export class BookingService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newBooking = new Booking(
       Math.random.toString(),
       placeId,
@@ -37,14 +39,19 @@ export class BookingService {
       dateFrom,
       dateTo
     );
-    return this.bookings.pipe(
+    return this.http.post<{ name: string }>(
+      'https://maxionicplaces.firebaseio.com/bookings.json',
+      { ...newBooking, id: null }
+    ).pipe(
+      switchMap(resData => {
+        generatedId = resData.name;
+        return this.bookings;
+      }),
       take(1),
-      delay(1000),
-      tap(
-        bookings => {
-          this._bookings.next(bookings.concat(newBooking));
-        }
-      )
+      tap(bookings => {
+        newBooking.id = generatedId;
+        this._bookings.next(bookings.concat(newBooking));
+      })
     );
   }
 
